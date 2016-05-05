31d116573c077dd4081144c0a3a1e8edaf4597d8
Philip <prkassel@gmail.com>
2016-05-05 02:14:58 -0500

    unix time stamp works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/timestamp', function(req, res) {
+    res.render('timestamp', {title: 'Time Stamp'});
+});
+
+router.get('/timestamp/:query', function(req, res) {
+  moment.updateLocale('en', {
+    months : [
+        "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
+    ]
+});
+  var query = req.params.query;
+  var isnum = /^\d+$/.test(query);
+  if (isnum) {
+  var unixDate = moment.unix(+query);
+  res.send('unix: ' + query + ', natural: ' + unixDate.format("MMMM Do, YYYY"));
+}
+
+else {
+  var naturalDate = moment(query, "MM-DD-YYYY");
+  res.send('natural: ' + naturalDate);
+}
 });
 
 module.exports = router;
